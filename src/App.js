b3cb5d1f321424a49bbc694edf00ea69514d2649
Dominik Lewicki <dominik_03-06@wp.pl>
2024-03-08 20:39:15 +0100

    qqq

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import O1 from "./components/O1";
 import O2 from "./components/O2"; 
 import O3 from "./components/O3"; 
 import O4 from "./components/O4"; 
+import O5 from "./components/O5"; 
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <O2/>
       <O3/>
       <O4/>
+      <O5/>
       
       
     </>
---
+++ src/components/O5.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import "./O5.css";
+import { FaInstagramSquare } from "react-icons/fa";
+
+
+import { FaSquareFacebook } from "react-icons/fa6";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaGithubSquare } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
+function O5() {
+  return (
+    <div className="o5">
+        <div className="k5">
+<div className="left5">
+    <p className='big-txt5'>REACT.</p>
+    <br></br>
+    <p className='small-txt5'>Lorem ipsum dolor, sit amet consectetur adipisicing<br></br> elit. Hic sint dolorem et numquam,<br></br>  consequuntur accusantium</p>
+    <br></br>
+    <div className="socials">
+        <div className="kwa-social"><FaInstagramSquare />
+
+</div>
+        <div className="kwa-social">
+        <FaSquareFacebook />
+        </div>
+        <div className="kwa-social">
+        <FaSquareXTwitter />
+        </div>
+        <div className="kwa-social">
+        <FaGithubSquare />
+
+        </div>
+        <div className="kwa-social">
+        <FaLinkedin />
+
+        </div>
+    </div>
+</div>
+<div className="right5">
+    <div className="for-ul">
+        <div className='linki'>Home</div>
+        <div className='linki'>Home</div>
+        <div className='linki'>Home</div>
+        <div className='linki'>Home</div>
+    </div>
+    <div className="for-ul">
+    <div className='linki'>Home</div><div className='linki'>Home</div><div className='linki'>Home</div><div className='linki'>Home</div>
+    </div>
+    <div className="for-ul">
+    <div className='linki'>Home</div><div className='linki'>Home</div><div className='linki'>Home</div><div className='linki'>Home</div>
+    </div>
+    <div className="for-ul">
+    <div className='linki'>Home</div><div className='linki'>Home</div><div className='linki'>Home</div><div className='linki'>Home</div>
+    </div>
+</div>
+        </div>
+    </div>
+  )
+}
+
+export default O5
