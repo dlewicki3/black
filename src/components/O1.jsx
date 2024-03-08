a4d172e6f6152039c25c2660099007ca22591e27
Dominik Lewicki <dominik_03-06@wp.pl>
2024-03-08 11:35:33 +0100

    vvvv

--- src/components/O1.jsx
+++ src/components/O1.jsx
@@ -4,14 +4,7 @@ import {motion} from 'framer-motion';
 function O1() {
   return (
    <div className="o1">
-  <div className="k1">
-    <p className='color-txt' >GROWING WITH DATA ANALYTICS</p>
-    <p className='big-txt-1' >Grow with data</p>
-    <p className='middle-txt-1' >Fast, flexible, financiong for SaaS</p>
-    <p className='small-txt-1'  >Monitor your data analytics to increas revenue for BTB, BTC,<br></br> SASS platforms</p>
-    <button className='big-btn-1'
-    >Get Started</button>
-  </div>
+  
    </div>
   )
 }
